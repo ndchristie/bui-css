972ee74e067ca6f371512c2aebda9cbfecad78b7
Nathan Christie <ndchristie@gmail.com>
2017-04-10 21:02:32 -0700

    add postcss plugins to reduce filesize and create core context mixin

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,44 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
-const moduleImporter = require('sass-module-importer');
 const del = require('del');
+// postcss block
+const postcss = require('gulp-postcss');
+const cssnext = require("postcss-cssnext");
+const normalizeCharset = require('postcss-normalize-charset');
+const minifySelectors = require('postcss-minify-selectors');
+const minifyFontValues = require('postcss-minify-font-values');
+const mergeLonghand = require('postcss-merge-longhand');
+const mergeIdents = require('postcss-merge-idents');
+const discardUnused = require('postcss-discard-unused');
+const discardEmpty = require('postcss-discard-empty');
+const discardDuplicates = require('postcss-discard-duplicates');
+const discardComments = require('postcss-discard-comments');
+const convertValues = require('postcss-convert-values');
+const mergeSelectors = require('postcss-merge-selectors');
+const cssnano = require("cssnano");
+
+const plugins = [
+  cssnext(),
+  normalizeCharset(),
+  mergeSelectors(), // unsafe, test assertively
+  minifySelectors(),
+  minifyFontValues(),
+  mergeLonghand(),
+  mergeIdents(),
+  discardUnused(),
+  discardEmpty(),
+  discardDuplicates(),
+  discardComments(),
+  convertValues(),
+];
 
 gulp.task('css', function () {
   return gulp.src('src/stylesheets/bui.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      importer: moduleImporter(),
-    }).on('error', sass.logError))
-    .pipe(postcss([require("postcss-cssnext")()]))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('lib/css'));
 });
@@ -20,7 +46,7 @@ gulp.task('css', function () {
 gulp.task('css:min', function () {
   return gulp.src('lib/**/!(*.min).css')
     .pipe(sourcemaps.init())
-    .pipe(postcss([require("cssnano")({ autoprefixer: false })]))
+    .pipe(postcss([cssnano({ autoprefixer: false })]))
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('lib'));
